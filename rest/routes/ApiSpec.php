7166a132b6faacc5fe8decf29ec212f1542a1658
ilmadzaferovic <ilma.dzaferovic@stu.ibu.edu.ba>
2023-06-11 15:31:16 +0200

    last touches part 2

--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -5,7 +5,7 @@
  * @OA\Info(title="The Reading Journal", version="0.1", @OA\Contact(email="ilma.dzaferovic@stu.ibu.edu.ba", name="Ilma Dzaferovic"))
  * @OA\OpenApi(
  *    @OA\Server(url="http://localhost/the_reading_journal/rest", description="Development Environment" ),
- *    @OA\Server(url="https://the_reading_journal.ba/rest", description="Production Environment" )
+ *    @OA\Server(url="https://the-reading-journal.vercel.app/rest", description="Production Environment" )
  * ),
  * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
  */
